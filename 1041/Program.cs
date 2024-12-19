4b74ad90f51ac359cb185e11abb3c9dd10c5dbab
Wilber Ribeiro <99516279+WilberR01@users.noreply.github.com>
2024-12-19 19:23:55 -0300

    1041 - v1
    Encontra o quadrante (condicionais)
    Sem validação, entradas padronizadas

---
+++ 1041/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+string [] coordenadas = Console.ReadLine().Split(' ');
+
+float X = float.Parse(coordenadas[0],CultureInfo.InvariantCulture);
+float Y = float.Parse(coordenadas[1],CultureInfo.InvariantCulture);
+
+if(X == 0 && Y == 0)
+{
+    Console.WriteLine("Origem");
+}
+else if(X == 0 && Y != 0)
+{
+    Console.WriteLine("Eixo Y");
+}
+else if(X != 0 && Y == 0)
+{
+    Console.WriteLine("Eixo X");
+}
+else if(X > 0 && Y > 0)
+{
+    Console.WriteLine("Q1");
+}
+else if(X < 0 && Y > 0)
+{
+    Console.WriteLine("Q2");
+}
+else if(X < 0 && Y < 0)
+{
+    Console.WriteLine("Q3");
+}
+else
+{
+    Console.WriteLine("Q4");
+}
