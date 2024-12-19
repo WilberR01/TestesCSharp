fceca160e1e80c417588550744dc63cbc7d3cb14
Wilber Ribeiro <99516279+WilberR01@users.noreply.github.com>
2024-12-19 19:03:47 -0300

    1020 - v1
    Expandindo dias em anos, meses e dias.

---
+++ 1020/Program.cs
@@ -0,0 +1,17 @@
+﻿int ano, mes, dia, diasUsuario;
+
+if (!int.TryParse(Console.ReadLine(), out diasUsuario))
+{
+    Console.WriteLine("Por favor, insira um número válido.");
+    return;
+}
+
+ano = diasUsuario / 365;
+diasUsuario = diasUsuario % 365;
+
+mes = diasUsuario / 30;
+diasUsuario = diasUsuario % 30;
+
+dia = diasUsuario;
+
+Console.WriteLine($"{ano} ano(s)\n{mes} mes(es)\n{dia} dia(s)");
