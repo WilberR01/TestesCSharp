76fd19971a8a4ffb6173770880e0f1675bb97905
Wilber Ribeiro <99516279+WilberR01@users.noreply.github.com>
2024-12-19 19:57:31 -0300

    1145 - v1
    Lista de número com intervalo

---
+++ 1145/Program.cs
@@ -0,0 +1,18 @@
+﻿string [] valores = Console.ReadLine().Split(' ');
+
+int X = int.Parse(valores[0]);
+int Y = int.Parse(valores[1]);
+
+for(int i = 1; i <= Y; i++)
+{
+    Console.Write(i);
+
+    if(i%X == 0)
+    {
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.Write(" ");
+    }
+}
